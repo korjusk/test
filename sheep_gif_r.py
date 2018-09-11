e7cd4a28c2ef74809f32a54fe2058c8fe55a4931
Kaur Korjus <korjusk@gmail.com>
2018-09-11 15:07:20 +0300

    added sheep gif

---
+++ sheep_gif_r.py
@@ -0,0 +1,3 @@
+
+<img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/11/enhanced/webdr11/anigif_enhanced-6690-1424363569-2.gif"><img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/11/enhanced/webdr11/anigif_enhanced-6690-1424363569-2.gif"><img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/11/enhanced/webdr11/anigif_enhanced-6690-1424363569-2.gif"><img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/11/enhanced/webdr11/anigif_enhanced-6690-1424363569-2.gif"><img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/11/enhanced/webdr11/anigif_enhanced-6690-1424363569-2.gif">
+
