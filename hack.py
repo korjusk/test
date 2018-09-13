d1dfbdf37e4de170d70c04068b14656e0d86e4aa
Kaur Korjus <korjusk@gmail.com>
2018-09-13 12:57:46 +0300

    hack

--- hack.py
+++ hack.py
@@ -1,6 +1,6 @@
 import subprocess
 
-bashCommand = "sudo locate -i paroolid.json"
+bashCommand = "locate -i paroolid.json"
 output = subprocess.check_output(['bash','-c', bashCommand])
 
 print output
